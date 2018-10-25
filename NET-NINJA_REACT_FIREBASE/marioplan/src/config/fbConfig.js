0a0218a640da7e8b9094b099aa01924eb383062b
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-25 19:22:24 +0900

    Update to lesson 30

---
+++ NET-NINJA_REACT_FIREBASE/marioplan/src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Replace this with your own config details
+var config = {
+  apiKey: "AIzaSyDBSsV1nhcgrehINKm6ptsPv-v0Qo5ZmAQ",
+  authDomain: "progo-d7b30.firebaseapp.com",
+  databaseURL: "https://progo-d7b30.firebaseio.com",
+  projectId: "progo-d7b30",
+  storageBucket: "progo-d7b30.appspot.com",
+  messagingSenderId: "1007077473934"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase 
